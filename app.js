3ba45bde3a9787079bcad0ab7ae69a3f7f284f5a
tbricca <tbricca@gmail.com>
2018-11-03 16:17:30 -0700

    added home.ejs file

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ const app = express();
 // set up view engine 
 app.set('view engine', 'ejs');
 
+//create home route 
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
 app.listen(3000, () =>{
     console.log('app now listening for requests on port 3000');
 });
